2bfb42474fb82878d21a73f337d8760be5cf625b
seku <sekupyna@gmail.com>
2009-06-12 15:44:16 +0200

    mailer3

--- app/models/login_link.rb
+++ app/models/login_link.rb
@@ -2,7 +2,7 @@ class LoginLink < ActionMailer::Base
   
   def login_link(user)
     subject       "Password Reset Instructions"
-    from          "movie_pole"
+    from          "trix@trix.megiteam.pl"
     recipients    user.email
     sent_on       Time.now
     body          :login_link => new_user_session_url(:token => user.perishable_token)
