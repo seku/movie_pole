b762157f690ed615e52601cc3a8ab5b461c0dde4
seku <sekupyna@gmail.com>
2009-06-12 14:55:14 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ config.action_controller.perform_caching             = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_charset = 'utf-8' 
   config.action_mailer.default_url_options = { :host => "trix.megiteam.pl" } 
