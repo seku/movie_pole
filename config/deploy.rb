9e0581d794d3dba2fab4aaad2eebaa13e55b2b4d
seku <sekupyna@gmail.com>
2009-06-12 10:26:44 +0200

    de

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-require 'thinking_sphinx/deploy/capistrano'
-
 set :application, "trix"
 set :repository,  "git://github.com/seku/movie_pole.git"
 set :scm, :git
@@ -18,6 +16,23 @@ namespace :deploy do
     task :restart, :role => :app do
     run "restart-app #{application}"
   end
+  
+  task :before_update do
+    # Stop Thinking Sphinx before the update so it finds its configuration file.
+    thinking_sphinx.stop
+  end
+
+  task :after_update do
+    symlink_sphinx_indexes
+    thinking_sphinx.configure
+    thinking_sphinx.start
+  end
+
+  desc "Link up Sphinx's indexes."
+  task :symlink_sphinx_indexes, :roles => [:app] do
+    run "ln -nfs #{shared_path}/db/sphinx #{current_path}/db/sphinx"
+  end
+  
 end
 
 namespace :db do
@@ -25,10 +40,34 @@ namespace :db do
   task :symlink do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
   end
+  
 end
 
+task :symlink_sphinx_yml do
+  run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/database.yml" 
+end
+
+namespace :thinking_sphinx do
+
+  task :configure, :roles => [:app] do
+    run "cd #{current_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
+  end
+  task :index, :roles => [:app] do
+    run "cd #{current_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
+  end
+  task :start, :roles => [:app] do
+    run "cd #{current_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+  end
+  task :stop, :roles => [:app] do
+    run "cd #{current_path}; rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
+  end
+  task :restart, :roles => [:app] do
+    run "cd #{current_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
+  end
+end
 after :deploy, "deploy:restart"
 after "deploy:update_code", "db:symlink"
+after "deploy:update_code", "symlink_sphinx_yml"
 
-after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
+
 
