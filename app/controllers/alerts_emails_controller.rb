d01b9dc202cae134b13aa7433b433708fbb2d970
seku <sekupyna@gmail.com>
2009-06-16 08:50:59 +0200

    alerts test

--- app/controllers/alerts_emails_controller.rb
+++ app/controllers/alerts_emails_controller.rb
@@ -6,7 +6,7 @@ class AlertsEmailsController < ApplicationController
     #redirect_to user_path(current_user)  #this line should be removed 
   end
   
-  def send_email_with_desirable_movies
+  def self.send_email_with_desirable_movies
     User.all.each do |user|
       @movies = []
       user.desirable_alerts.each do |alert|
@@ -20,7 +20,7 @@ class AlertsEmailsController < ApplicationController
     end
   end
   
-  def send_weekly_alerts
+  def self.send_weekly_alerts
     User.all.each do |user|
       @movies = []
       user.weekly_alerts.each do |alert|
@@ -30,19 +30,19 @@ class AlertsEmailsController < ApplicationController
     end
   end
 
-  def alert_movies(alert) #should be changed to finding only movies from this week 
+  def self.alert_movies(alert) #should be changed to finding only movies from this week 
     @genre = Genre.find(alert.genre_id) 
     @movies = @genre.movies.rated_with(alert.rating)
     @movies = @movies.only_torrents.limited(3)
     @movies = @movies.sorted_by("release_date", "desc")  
   end
 
-  def send_email(user, movies)
+  def self.send_email(user, movies)
     MoviesEmail.deliver_movies_email(user, movies)
     flash[:notice] = "email sended" #should be removed
   end
   
-  def send_desirable_email(user, movies)
+  def self.send_desirable_email(user, movies)
     MoviesEmail.deliver_desirable_email(user, movies)
     flash[:notice] = "#{user.email} email sended"  #should be removed
   end 
