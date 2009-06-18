86766027caa76529f44d3271bfe10482d828c358
seku <sekupyna@gmail.com>
2009-06-18 12:59:31 +0200

    correct torrents

--- app/models/torrent.rb
+++ app/models/torrent.rb
@@ -5,12 +5,13 @@ class Torrent < ActiveRecord::Base
 	def self.fetch_torrents
     puts "..update_all_torrents.."
     torrents = Movie.all.map do |m| 
-    	#unless m.torrents.size > 0   #0 or 3 ??
-    	results =	@torrent.from_imdb(m.imdb_id)
-    	results.each do |t|
-        if t[:size] > Torrent_limit_size
-        	m.torrents.create!(:url => t[:url], :size => t[:size], :title => t[:title], :leechers => t[:leechers], :seeders => t[:seeds])
-      	end
+    	unless m.torrents.size > 3   #0 or 3 ??
+    	  results =	@torrent.from_imdb(m.imdb_id)
+    	  results.each do |t|
+          if t[:size] > Torrent_limit_size
+        	  m.torrents.create!(:url => t[:url], :size => t[:size], :title => t[:title], :leechers => t[:leechers], :seeders => t[:seeds])
+      	  end
+        end
       end
     end    
   end
