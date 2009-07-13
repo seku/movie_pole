e72aa49cf0bf16dec72c1438180fcfb12a878704
seku <sekupyna@gmail.com>
2009-07-13 12:53:59 +0200

    locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  helper :all
   filter_parameter_logging :password, :password_confirmation
-  #helper_method :current_user_session, :current_user
+
   include Helpers
-  # See ActionController::RequestForgeryProtection for details
+
   protect_from_forgery
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+
   before_filter :set_locale, :set_defaults
   
   def set_defaults
@@ -23,14 +19,13 @@ class ApplicationController < ActionController::Base
   end
   
   def set_locale
-    I18n.locale = cookies[:locale] || :en
+    @locale = cookies[:locale] || "en"
+    I18n.locale = @locale
   end
 
   def per_page #pagination number
     10
-  end
-
-    
+  end  
 
 
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-	before_filter :set_locale
+	
 	def index
     @genre = Genre.find(params[:genre_id], :include => { :movies => :torrents})
     @movies = @genre.movies.rated_with(params[:rating] || 1)
@@ -7,11 +7,9 @@ class MoviesController < ApplicationController
     @movies = @movies.sorted_by((params[:sort_by] || "release_date"), (@sort_mode[params[:sort_by]] || "desc"))
     @movies = @movies.paginate :page => params[:page], :per_page => per_page
   end
+  
   def show
     @movie = Movie.find(params[:id], :include => :torrents) 
   end
   
-  def set_locale
-    I18n.locale = cookies[:locale] || :en
-  end 
 end
